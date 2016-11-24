57e5dca1758b9b6941695154841256eb8a86d9c6
Luis Lopez <luigibyte@gmail.com>
2016-11-23 21:04:48 -0300

    devices dashboard (wip)

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+
+  def show
+    @devices = Device.all
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :devices
 
+  get 'dashboard' => 'dashboard#show', as: :dashboard
+
   root to: 'devices#index'
 end
