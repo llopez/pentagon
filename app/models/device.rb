9029c44ff5ecd35e9b56ef5c4ce7878d9bb591d2
Luis Lopez <luigibyte@gmail.com>
2016-12-01 15:12:49 -0300

    mqtt client: send message when device state changes

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,9 @@
 class Device < ApplicationRecord
+  before_save :send_message, if: :state_changed?
+
+  def send_message
+    MQTT::Client.connect(Rails.application.secrets.mqtt_server) do |c|
+      c.publish("device/#{id}", state)
+    end
+  end
 end
