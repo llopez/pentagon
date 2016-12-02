9bd86076871637acb82c2fdc1b7d64b7bb8a5448
Luis Lopez <luigibyte@gmail.com>
2016-12-02 17:02:14 -0300

    added some notes

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,9 +1,15 @@
 class Device < ApplicationRecord
   before_save :send_message, if: :state_changed?
 
+  # state: on, off, turning_on, turning_off
+
   def send_message
     MQTT::Client.connect(Rails.application.secrets.mqtt_server) do |c|
       c.publish("device/#{id}", state)
+      #puts c.get("device/#{id}/ack")
     end
   end
 end
+
+# before_save -> background(sub -t device/:id/ack) ActiveJob + sidekiq
+# after_save -> pub -t device/:id/control -m ON
