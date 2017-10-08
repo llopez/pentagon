5c2fb2f6f1acbbd74dbed1f640279c8eccb3037a
Luis Lopez <luigibyte@gmail.com>
2017-10-08 16:21:01 -0300

    add propagate state callback add presence validation over name attr add set defaults callback

--- app/models/switch.rb
+++ app/models/switch.rb
@@ -1,2 +1,10 @@
 class Switch < Device
+  before_save :send_message, if: :state_changed?
+  after_initialize :set_default_values
+
+  validates :name, :state, presence: true
+
+  def set_default_values
+    self.state ||= 'off'
+  end
 end
